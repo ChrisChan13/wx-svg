1974ae6fc8d384b198c0272bf765f6333aba4aba
ChrisChan <chrischan1213@qq.com>
2023-12-13 09:21:33 +0800

    v1.0.0

---
+++ tools/demo/pages/index/index.js
@@ -0,0 +1,11 @@
+Page({
+  data: {
+    // eslint-disable-next-line no-sparse-arrays
+    colorsArray: [
+      , 'midnightblue', , , 'midnightblue', , 'midnightblue', 'mediumseagreen', , 'mediumseagreen', , 'midnightblue', 'hotpink', 'orangered', 'mediumseagreen', , 'mediumseagreen',
+    ],
+    colorsObject: {
+      '#6c63ff': 'crimson', '#2f2e41': 'darkslategray', '#dddedf': 'gold', 12: 'orangered',
+    },
+  },
+});
